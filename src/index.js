64f5c39a9790ea3daf04ca0edd93797f584a323d
alex <sakun.alexey@gmail.com>
2021-09-17 16:01:22 +0300

    feat: create App component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app/app';
 import './assets/scss/style.scss';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
